9f09effa1daa752d2284226cf4222cd5a0452949
Scott <scott.harpster@gmail.com>
2020-07-31 21:42:02 -0600

    Update easyping.cron.sh
    fixing an error with carton and the cron script.

--- easyping.cron.sh
+++ easyping.cron.sh
@@ -2,9 +2,10 @@
 #Adjust path to match your system
 #whereis carton
 CARTON_EXEC=/usr/bin/carton
-EASYPING_EXEC=/path/to/EasyPing/easyping.pl
+EASYPING_PATH=/path/to/EasyPing
+cd $EASYPING_PATH
 if [ $# -eq 0 ]; then
-$CARTON_EXEC exec $EASYPING_EXEC -c
+$CARTON_EXEC exec $EASYPING_PATH/easyping.pl -c
 else
-$CARTON_EXEC exec $EASYPING_EXEC -c -g $1
+$CARTON_EXEC exec $EASYPING_PATH/easyping.pl -c -g $1
 fi
